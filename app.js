b3229fbd9839c42ee330472c2a00ba031df3b32a
dolook2 <dolook2@naver.com>
2020-12-03 22:07:47 +0900

    resize and erase add

--- app.js
+++ app.js
@@ -1,103 +1,189 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("jsColor");
-const range = document.getElementById("jsRange");
-const mode = document.getElementById("jsMode");
-const saveBtn = document.getElementById("jsSave");
-
+// Constant
 const INITIAL_COLOR = "#2c2c2c";
-const CANVAS_SIZE = 700;
-
-canvas.width = CANVAS_SIZE;
-canvas.height = CANVAS_SIZE;
-
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-ctx.strokeStyle = INITIAL_COLOR;
-ctx.fillStyle = INITIAL_COLOR;
-ctx.lineWidth = 2.5;
-
-let painting = false;
-let filling = false;
-
-function stopPainting() {
-    painting = false;
-}
-
-function startPainting() {
-    painting = true;
-}
-
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (!painting) {
+const INITIAL_BG_COLOR = "white";
+const INITIAL_LINE_WIDTH = 2.5;
+const BTN_CLICKED_CN = "controls__color__clicked";
+// Dom Element
+const canvasParent = document.querySelector("#canvas");
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColor");
+const myColorContorls = document.querySelector("#jsMyColorControl");
+const myColor = document.querySelector("#jsMyColor");
+const range = document.querySelector("#jsRange");
+const mode = document.querySelector("#jsMode");
+const saveBtn = document.querySelector("#jsSave");
+const resetBtn = document.querySelector("#jsReset");
+const resizeBtn = document.querySelector("#jsResize");
+const widthControls = document.querySelector("#jsWidth");
+const heightControls = document.querySelector("#jsHeight");
+const erase = document.querySelector("#jsErase");
+// Variable
+let canvasWidth = 600;
+let canvasHeight = 500;
+let isPainting = false;
+let isFilling = false;
+// Init setting
+const initSetting = () => {
+    // Set width, height of canvas
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    // Set background color, paint color, fill color, line width of canvas
+    ctx.fillStyle = INITIAL_BG_COLOR;
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.strokeStyle = INITIAL_COLOR;
+    ctx.fillStyle = INITIAL_COLOR;
+    ctx.lineWidth = INITIAL_LINE_WIDTH;
+    // Set initial line width
+    range.value = INITIAL_LINE_WIDTH;
+    // Set initial mode to paint
+    isFilling = false;
+    mode.innerText = "fill";
+    // Set all button unclicked
+    colors.forEach((color) => {
+        color.classList.remove(BTN_CLICKED_CN);
+    });
+    // Set black button clicked
+    colors[0].classList.add(BTN_CLICKED_CN);
+};
+// Init event
+const initEvent = () => {
+    // Add event to Canvas
+    if (canvas) {
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mouseleave", stopPainting);
+        canvas.addEventListener("mouseup", stopPainting);
+        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("click", handleCanvasClick);
+        canvas.addEventListener("contextmenu", handleContextMenu);
+    }
+    // Add event to color
+    colors.forEach((color) => {
+        color.addEventListener("click", handleColorClick);
+    });
+    // Add event to range
+    if (range) {
+        range.addEventListener("input", handleRangeChange);
+    }
+    // Add event to mode button
+    if (mode) {
+        mode.addEventListener("click", hanldeModeClick);
+    }
+    // Add event to save button
+    if (saveBtn) {
+        saveBtn.addEventListener("click", handleSaveClick);
+    }
+    // Add event to reset button
+    if (resetBtn) {
+        resetBtn.addEventListener("click", handleResetClick);
+    }
+    // Add event to my color
+    if (myColorContorls) {
+        myColorContorls.addEventListener("change", handleMyColorChange);
+    }
+    // Add event to resize button
+    if (resizeBtn) {
+        resizeBtn.addEventListener("click", handleResizeClick);
+    }
+    if (erase) {
+        erase.addEventListener("click", handleEraseClick);
+    }
+};
+// Set start paint
+const startPainting = () => {
+    isPainting = true;
+};
+// Set stop paint
+const stopPainting = () => {
+    isPainting = false;
+};
+// Event of move mouse on canvas
+const onMouseMove = (e) => {
+    if (isFilling) return;
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if (!isPainting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
-}
-
-function handleColorClick(event) {
-    const color = event.target.style.backgroundColor;
+};
+// Event of click color
+const handleColorClick = (e) => {
+    // set color of paint or fill
+    const color = e.target.style.backgroundColor;
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
-}
-
-function handleRangeChange(event) {
-    const size = event.target.value;
+    // Set all button unclicked
+    colors.forEach((color) => {
+        color.classList.remove(BTN_CLICKED_CN);
+    });
+    // Set clicked button clicked
+    e.target.classList.add(BTN_CLICKED_CN);
+};
+// Event of change line width
+const handleRangeChange = (e) => {
+    const size = e.target.value;
     ctx.lineWidth = size;
-}
-
-function handleModeClick() {
-    if (filling === true) {
-        filling = false;
-        mode.innerText = "Fill";
+};
+// Event of change mode
+const hanldeModeClick = () => {
+    if (isFilling === true) {
+        // If current mode is paint
+        isFilling = false;
+        mode.innerText = "fill";
     } else {
-        filling = true;
-        mode.innerText = "Paint";
+        // If current mode is fill
+        isFilling = true;
+        mode.innerText = "paint";
     }
-}
-
-function handleCanvasClick() {
-    if (filling) {
-        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+};
+// Event of click canvas
+const handleCanvasClick = () => {
+    if (isFilling === true) {
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     }
-}
-
-function handleCM(event) {
-    event.preventDefault();
-}
-
-function handleSaveClick() {
-    const image = canvas.toDataURL("image/png");
+};
+// Prevent event of right click
+const handleContextMenu = (e) => {
+    e.preventDefault();
+};
+// Event of Click save button
+const handleSaveClick = () => {
+    const image = canvas.toDataURL();
     const link = document.createElement("a");
     link.href = image;
     link.download = "MyPainting🎨";
     link.click();
-}
+};
+// Event of Click reset button
+const handleResetClick = () => {
+    initSetting();
+};
+// Event of Change my color
+const handleMyColorChange = (e) => {
+    const color = e.target.value;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    myColor.style.backgroundColor = color;
+};
+// Event of Change size of canvas
+const handleResizeClick = (e) => {
+    if (widthControls.value > window.innerWidth) {
+        alert("Too Large");
+    } else {
+        canvasWidth = widthControls.value;
+        canvasHeight = heightControls.value;
+        initSetting();
+    }
+};
 
-if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick);
-    canvas.addEventListener("contextmenu", handleCM);
-}
-
-Array.from(colors).forEach((color) => color.addEventListener("click", handleColorClick));
-
-if (range) {
-    range.addEventListener("input", handleRangeChange);
-}
-
-if (mode) {
-    mode.addEventListener("click", handleModeClick);
-}
-
-if (saveBtn) {
-    saveBtn.addEventListener("click", handleSaveClick);
-}
+handleEraseClick = (e) => {
+    painting = true;
+    ctx.strokeStyle = "white";
+};
+// init
+initSetting();
+initEvent();
